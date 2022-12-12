6ca695cdeeedd3255366531ccd822ba82e497ad9
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2022-12-13 03:19:10 +0530

    feat: initialize npm package

--- client/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./creator-actions/creator-actions"
-export * from "./queries/subscription-queries"
-export * from "./config/config"
-export * from "./queries/tier-queries"
--- client/src/queries/subscription-queries.ts
+++ client/src/queries/subscription-queries.ts
@@ -1,95 +1,98 @@
-import { web3 } from "@project-serum/anchor"
-import { getProgram } from "../config/config"
-import { subscriptionAccountKey } from "../utils/pda-derivations"
-import { getAllTiersForApp } from "./tier-queries"
+import { web3 } from "@project-serum/anchor";
+import { getProgram } from "../config/config";
+import { subscriptionAccountKey } from "../utils/pda-derivations";
+import { getAllTiersForApp } from "./tier-queries";
 
-const program = getProgram()
+const program = getProgram();
 
 export async function getAllSubscriptionsToApp(app: web3.PublicKey) {
   const subscriptions = await program.account.subscription.all([
     { memcmp: { offset: 8, bytes: app.toBase58() } },
-  ])
+  ]);
 
-  return subscriptions
+  return subscriptions;
 }
 
 export async function getAllActiveSubscriptionsToApp(app: web3.PublicKey) {
-  let subscriptions = await getAllSubscriptionsToApp(app)
+  let subscriptions = await getAllSubscriptionsToApp(app);
 
   subscriptions = subscriptions.filter((sub) => {
     return (
       sub.account.payPeriodExpiration.toNumber() > new Date().getTime() / 1000
-    )
-  })
+    );
+  });
 
-  return subscriptions
+  return subscriptions;
 }
 
 export async function getAllSubscriptionsToTier(tier: web3.PublicKey) {
   const subscriptions = await program.account.subscription.all([
     { memcmp: { offset: 40, bytes: tier.toBase58() } },
-  ])
+  ]);
 
-  return subscriptions
+  return subscriptions;
 }
 
 export async function getAllActiveSubscriptionsToTier(tier: web3.PublicKey) {
-  let subscriptions = await getAllSubscriptionsToTier(tier)
+  let subscriptions = await getAllSubscriptionsToTier(tier);
 
-  let now = new Date().getTime() / 1000
+  let now = new Date().getTime() / 1000;
   subscriptions = subscriptions.filter((sub) => {
-    return sub.account.payPeriodExpiration.toNumber() > now
-  })
+    return sub.account.payPeriodExpiration.toNumber() > now;
+  });
 
-  return subscriptions
+  return subscriptions;
 }
 
 export async function getActiveSubscriptionCountForTier(tier: web3.PublicKey) {
-  const subscriptions = await getAllActiveSubscriptionsToTier(tier)
-  return subscriptions.length
+  const subscriptions = await getAllActiveSubscriptionsToTier(tier);
+  return subscriptions.length;
 }
 
 export async function getActiveSubscriptionCountForApp(app: web3.PublicKey) {
-  const subscriptions = await getAllActiveSubscriptionsToApp(app)
+  const subscriptions = await getAllActiveSubscriptionsToApp(app);
 
-  return subscriptions.length
+  return subscriptions.length;
 }
 
 export async function getActiveSubscriptionsToAppGroupedByTier(
   app: web3.PublicKey
 ) {
-  let subscriptions = await getAllActiveSubscriptionsToApp(app)
+  let subscriptions = await getAllActiveSubscriptionsToApp(app);
 
-  let grouped = {}
+  let grouped = {};
 
   subscriptions.map((sub) => {
+    // @ts-ignore
     if (grouped[sub.account.tier.toBase58()]) {
-      grouped[sub.account.tier.toBase58()].push(sub)
+      // @ts-ignore
+      grouped[sub.account.tier.toBase58()].push(sub);
     } else {
-      grouped[sub.account.tier.toBase58()] = [sub]
+      // @ts-ignore
+      grouped[sub.account.tier.toBase58()] = [sub];
     }
-  })
+  });
 
-  return grouped
+  return grouped;
 }
 
 export async function getAllSubscriptionsForUser(user: web3.PublicKey) {
   const subscriptions = await program.account.subscription.all([
     { memcmp: { offset: 72, bytes: user.toBase58() } },
-  ])
+  ]);
 
-  return subscriptions
+  return subscriptions;
 }
 
 export async function getActiveSubscriptionsForUser(user: web3.PublicKey) {
-  let subscriptions = await getAllSubscriptionsForUser(user)
+  let subscriptions = await getAllSubscriptionsForUser(user);
 
-  let now = new Date().getTime() / 1000
+  let now = new Date().getTime() / 1000;
   subscriptions = subscriptions.filter((sub) => {
-    return sub.account.payPeriodExpiration.toNumber() > now
-  })
+    return sub.account.payPeriodExpiration.toNumber() > now;
+  });
 
-  return subscriptions
+  return subscriptions;
 }
 
 export async function isActiveSubscriber(
@@ -97,23 +100,23 @@ export async function isActiveSubscriber(
   app: web3.PublicKey,
   tier: web3.PublicKey | null = null
 ) {
-  let [key] = subscriptionAccountKey(subscriber, app)
-  let subscription = await program.account.subscription.fetch(key)
+  let [key] = subscriptionAccountKey(subscriber, app);
+  let subscription = await program.account.subscription.fetch(key);
 
   if (tier && subscription.tier !== tier) {
-    return
+    return;
   }
 
-  const now = new Date().getTime() / 1000
+  const now = new Date().getTime() / 1000;
 
-  return subscription.payPeriodExpiration.toNumber() > now
+  return subscription.payPeriodExpiration.toNumber() > now;
 }
 
 export async function getSubscription(
   subscriber: web3.PublicKey,
   app: web3.PublicKey
 ) {
-  let [key] = subscriptionAccountKey(subscriber, app)
-  let subscription = await program.account.subscription.fetch(key)
-  return subscription
+  let [key] = subscriptionAccountKey(subscriber, app);
+  let subscription = await program.account.subscription.fetch(key);
+  return subscription;
 }
